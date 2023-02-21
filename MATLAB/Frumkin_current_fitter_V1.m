dcdd75dfc4bd01fe32dc93ba7b22a9f35fbe4525
Moss <bm1114@ic.ac.uk>
2023-02-21 11:22:40 +0000

    changes

--- MATLAB/Frumkin_current_fitter_V1.m
+++ MATLAB/Frumkin_current_fitter_V1.m
@@ -1,29 +1,30 @@
 clc
 
 % before using get your data into a .mat format with E as X, theta as Y
-load('Co_data_J.mat')
+load('PB_J.mat')
 
 
-%fit_upper_k=10000;
-%fit_lower_k=0;
-%fit_upper_a=1;
-%fit_lower_a=0;
+fit_upper_k=10000;
+fit_lower_k=1E-8;
+fit_upper_a=1;
+fit_lower_a=0;
 
 theta=data(:,1);
 j_exp=data(:,2);
+
 
 [xData, yData] = prepareCurveData( theta, j_exp);
 %+0.0256*log(t/(1-t))+(b/96485)*t+U1
 % Set up fittype and options.
 % set values in equation to the ones from frumkin fit - 
-ft = fittype( 'k*t*exp(a*(1/0.0256)*0.36*t+0.14+(log(t/(1-t))))', 'independent', 't', 'dependent', 'E' );
+ft = fittype( 't*k*exp((-a*(t)+c))', 'independent', 't', 'dependent', 'E' );
 %excludedPoints = (xData < exclude_lower) | (xData > exclude_upper);
 %the above cuts the potential region where there might be noise in coverage
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 opts.Display = 'Off';
-opts.StartPoint = [1 1];
+opts.StartPoint = [0.2769 -0.02 0.1];
 %, 0.7, 1.55
-%opts.Lower = [ fit_lower_k fit_lower_a];
+opts.Lower = [  -inf -inf 1E-6];
 %opts.Upper = [fit_upper_k  fit_upper_a ];
 
 %opts.Exclude = excludedPoints;
@@ -39,6 +40,9 @@ disp(k_new);
 a_new=fittedmodel.a;
 disp('a value is (dimensionless)');
 disp(a_new);
+c_new=fittedmodel.c;
+disp('c value is (dimensionless)');
+disp(c_new);
 R_sq=fitresult.rsquare;
 disp('Rsq value is');
 disp(R_sq);
---
+++ MATLAB/Rate_law_interpolator.m
@@ -0,0 +1,119 @@
+%input data
+clc
+load('data.mat')
+%select data range
+x1=Data(:,1); 
+y1=Data(:,2);
+x2=Data(:,3);
+y2=Data(:,4);
+x3=Data(:,5);
+y3=Data(:,6);
+x4=Data(:,7);
+y4=Data(:,8);
+%set fitting tolerance, the less the best fitting for original data, but
+%more noisy for differential data
+fit_tolerance1= 8e-09;
+fit_tolerance2= 8e-09;
+fit_tolerance3= 9e-10;
+% Specify the smoothing parameter p that vary across the data.
+p=fit_tolerance3*[ones(1,560),0.001*ones(1,75),ones(1,465)];
+
+%x=x(1:end-250);  %delete noisy data at high potential
+%y1=y1(1:end-250);
+%y2=Data(:,3);
+%y2=y2(1:end-250);
+%y3=Data(:,4);
+%y3=y3(670:end);
+
+%estimate noise range and fitting 
+%sqrt(estimatenoise(y1));
+
+[sp1,y1_fit] = spaps(x1,y1,fit_tolerance1);
+fnplt(sp1);
+y1_fit=fnplt(sp1);
+y1_fit=y1_fit';
+
+
+[sp2,y2_fit] = spaps(x2,y2,fit_tolerance2);
+fnplt(sp2)
+y2_fit=fnplt(sp2);
+y2_fit=y2_fit';
+
+% choose another fitting method csaps for the linear line fitting, and control the smooth at different region.
+[sp3,y3_fit] = spaps(x3,y3,fit_tolerance3);
+fnplt(sp3)
+y3_fit=fnplt(sp3);
+y3_fit=y3_fit';
+
+
+%x_fit= 300:1:500 %choose the data arrange you need to get 
+%y1_fit=feval(spl,x_fit)
+%sqrt(estimatenoise(y2));
+%sp2 = spaps(x,y2,2e-08);
+%fnplt(sp2)
+
+
+%sqrt(estimatenoise(y3));
+%sp3 = spaps(x,y3,2e-08);
+%fnplt(sp3)
+
+
+figure (1)
+plot(x1,y1,'k.')
+hold on
+fnplt(sp1)
+plot(x2,y2,'k.')
+fnplt(sp2)
+plot(x3,y3,'k.')
+fnplt(sp3)
+hold off
+figure (2)
+hold on
+fnplt(fnder(sp1));
+y1_fit_dif=fnplt(fnder(sp1));
+y1_fit_dif=y1_fit_dif';
+fnplt(fnder(sp2));
+y2_fit_dif=fnplt(fnder(sp2));
+y2_fit_dif=y2_fit_dif';
+fnplt(fnder(sp3));
+y3_fit_dif=fnplt(fnder(sp3));
+y3_fit_dif=y3_fit_dif';
+hold off
+%figure (3)
+%fnplt(fnder(fnder(spl),1))
+%y1_fit_dif2=fnplt(fnder(fnder(spl),1))
+%y1_fit_dif2=y1_fit_dif2'
+y_fit=[y1_fit,y2_fit,y3_fit];
+
+% convert into array of original dimensions
+
+
+for i=1:length(x)
+n=x(i);
+fit_x=y_fit(:,1);
+[minim,ind]=min(abs(fit_x-n));
+ind=min(ind);
+y_fit_real(i,:)=y_fit(ind,:);
+
+end
+
+y_fit_dif=[y1_fit_dif,y2_fit_dif,y3_fit_dif];
+
+for i=1:length(x)
+n=x(i);
+fit_x=y_fit_dif(:,1);
+[minim,ind]=min(abs(fit_x-n));
+ind=min(ind);
+y_fit_diff_real(i,:)=y_fit_dif(ind,:);
+
+end
+fileN=strcat(filename,'_fit.csv');
+fileN1=strcat(filename,'_fit_dif.csv');
+%fileN2=strcat(filename,'_fit_dif2.csv');
+csvwrite(fileN,y_fit_real);
+csvwrite(fileN1,y_fit_diff_real);
+%csvwrite(fileN2,y1_fit_dif2);
+%hold on
+%fnplt(fnder(sp2))
+%fnplt(fnder(sp3))
+clear
